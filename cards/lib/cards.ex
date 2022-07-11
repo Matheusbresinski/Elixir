fec8c8378fb820879ff6f50595006d339039e61e
Matheus <matheusbresinski@hotmail.com>
2022-07-11 01:15:47 -0300

    An Elixir Warmup

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,6 +1,27 @@
 defmodule Cards do
 
   def create_deck do
-    ["ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+        "#{value} of #{suit}"
+    end
+
+  end
+
+  def shuffle(deck) do
+      Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
   end
 end
+#
+# Cards.deal(deck, 5) #{*hand*, *deck*} // tupla
+# Cards.deal(deck, 5) #{hand: [], deck:[]} // List

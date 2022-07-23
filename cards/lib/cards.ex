65124661ce851efcbfabe5848747a398f34096d2
Matheus <matheusbresinski@hotmail.com>
2022-07-22 22:14:26 -0300

    Elixir's Amazing Pattern Matching

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -21,6 +21,33 @@ defmodule Cards do
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term binary
+      {:error, _reason} -> "That file does not exist"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
+  end
+
+
+  # def load() do
+  #   {status, binary} = File.read(filename)
+  #
+  #   if(status === :error){
+  #     return "Something went wrong"
+  #   }
+  # end
 end
 #
 # Cards.deal(deck, 5) #{*hand*, *deck*} // tupla
